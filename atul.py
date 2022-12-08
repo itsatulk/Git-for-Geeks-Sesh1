071899802e365a78b8e48bf17fdd82101d3c46c1
itsatulk <120120348+itsatulk@users.noreply.github.com>
2022-12-08 21:56:43 +0530

    Update atul.py

--- atul.py
+++ atul.py
@@ -1,4 +1,4 @@
 print("ATUL")
 print("b22201")
 print("Electrical Engineering")
-print("I can sleep for a whole day and can still feel lazy")
+print("I can sleep for a whole day and still feel lazy")
